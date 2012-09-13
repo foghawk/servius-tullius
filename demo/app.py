2413b0d060ee69edb3b96ce45651a8d43c6d5dc9
Foghawk <scimitarfawkes@gmail.com>
2012-09-13 02:56:20 -0500

    one last hail mary against the internal server error

--- demo/app.py
+++ demo/app.py
@@ -6,10 +6,11 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask("demo")
 
 
-@app.route("/")
-def root():
-    return render_template("index.html")
-
 @app.teardown_request
 def shutdown_session(exception = None):
 	db.session.remove()
+	
+	
+@app.route("/")
+def root():
+    return render_template("index.html")
